c057cf2028b36d62aa3258490049387f40ac76d3
pquerols <pquerol@gmail.com>
2025-07-22 21:46:44 +0200

    new changes

--- layers/layer_user.js
+++ layers/layer_user.js
@@ -2,17 +2,10 @@
 import { Vector as VectorSource } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
 
-// this creates a new VectorSource (in-memory container for vector features). A place to store the goeometry drawn by the user
+// new VectorSource (in-memory container for vector features). A place to store the goeometry drawn by the user
 const userSource = new VectorSource();
-
-// this creates a new VectorLayer (in-memory layer for VectorSource)
+// new VectorLayer (in-memory layer for VectorSource)
 const userLayer = new VectorLayer({ source: userSource });
-
-// Este script era para cuando teniamos como input predefinido un Polygon
-// const userDraw = new Draw({
-//   source: userVector,
-//   type: 'Polygon', // or 'Point', 'LineString'
-// });
 
 export { userLayer, userSource};
 
